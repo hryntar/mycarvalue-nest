906b882471487485112417b9ebb5006c88f9a0ed
hryntar <hrynchuktt@gmail.com>
2025-01-31 17:54:50 +0200

    C:/Program Files/Git/signin implementation

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,6 @@
 import { randomBytes, scrypt as _scrypt } from "crypto";
 import { promisify } from "util";
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 const scrypt = promisify(_scrypt);
@@ -23,7 +23,17 @@ export class AuthService {
       return user;
    }
 
-   signin() {
+   async signin(email: string, password: string) {
+      const [user] = await this.usersService.find(email);
+      if (!user) {
+         throw new NotFoundException("user not found")
+      }
 
+      const [salt, storedHash] = user.password.split('.');
+      const hash = (await scrypt(password, salt, 32)) as Buffer;
+      if (storedHash !== hash.toString('hex')) {
+         throw new BadRequestException('bad password');
+      }
+      return user;
    }
 }
