2dd47e344bac5f1f903235f19988f514c55427b3
hryntar <hrynchuktt@gmail.com>
2025-02-01 17:25:39 +0200

    setting up sessions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Query, Param, Delete, NotFoundException, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Query, Param, Delete, NotFoundException, Session } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -12,13 +12,22 @@ export class UsersController {
    constructor(private usersService: UsersService, private authService: AuthService) { }
 
    @Post('/signup')
-   createUser(@Body() body: CreateUserDto) {
-      return this.authService.signup(body.email, body.password);
+   async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+      const user = await this.authService.signup(body.email, body.password);
+      session.userId = user.id;
+      return user;
    }
 
    @Post('/signin')
-   signin(@Body() body: CreateUserDto) {
-      return this.authService.signin(body.email, body.password);
+   async signin(@Body() body: CreateUserDto, @Session() session: any) {
+      const user = await this.authService.signin(body.email, body.password);
+      session.userId = user.id;
+      return user;
+   }
+
+   @Post('/signout')
+   signOut(@Session() session: any) {
+      session.userId = null;
    }
 
    @Get('/:id')
