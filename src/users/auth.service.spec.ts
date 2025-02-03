6723fd5b1152763ea68fa3f265dbda9002a74794
hryntar <hrynchuktt@gmail.com>
2025-02-03 17:32:53 +0200

    auth service unit tests refactoring

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -45,26 +45,23 @@ describe("AuthService", () => {
       expect(hash).toBeDefined();
    });
 
-   it("throws an error if user signs up with email in use", async () => {
-      fakeUsersService.find = () => Promise.resolve([{ id: 1, email: "a", password: "b" } as User])
+   it('throws an error if user signs up with email that is in use', async () => {
+      await service.signup('asdf@asdf.com', 'asdf');
       await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
          BadRequestException,
       );
-   })
+   });
 
    it('throws if signin is called with an unused email', async () => {
-      await expect(service.signin('asdf@asdf.com', "asdf")).rejects.toThrow(
-         NotFoundException,
-      )
-   })
+      await expect(
+         service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+      ).rejects.toThrow(NotFoundException);
+   });
 
    it('throws if an invalid password is provided', async () => {
-      fakeUsersService.find = () =>
-         Promise.resolve([
-            { email: 'asdf@asdf.com', password: 'laskdjf' } as User,
-         ]);
+      await service.signup('laskdjf@alskdfj.com', 'password');
       await expect(
-         service.signin('laskdjf@alskdfj.com', 'passowrd'),
+         service.signin('laskdjf@alskdfj.com', 'laksdlfkj'),
       ).rejects.toThrow(BadRequestException);
    });
 
