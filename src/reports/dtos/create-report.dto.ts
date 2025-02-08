b65026574624ca9989f17a6082f8bea8961d1abe
hryntar <hrynchuktt@gmail.com>
2025-02-08 16:03:55 +0200

    create report dto

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,30 @@
+import { IsNumber, Min, Max, IsString, IsLongitude, IsLatitude, max } from "class-validator";
+
+export class CreateReportDto {
+   @IsString()
+   make: string;
+
+   @IsString()
+   model: string;
+
+   @IsNumber()
+   @Min(1930)
+   @Max(2050)
+   year: number;
+
+   @IsNumber()
+   @Min(0)
+   @Max(1000000)
+   mileage: number;
+
+   @IsNumber()
+   @Min(0)
+   @Max(1000000)
+   price: number;
+
+   @IsLongitude()
+   lng: number;
+
+   @IsLatitude()
+   lat: number;
+}
