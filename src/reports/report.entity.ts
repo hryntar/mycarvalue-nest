e26d384e9eaa39e095f4f82964d36aa930156d8d
hryntar <hrynchuktt@gmail.com>
2025-02-09 11:27:48 +0200

    assotiations

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/users/user.entity";
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
 
 
 @Entity()
@@ -26,4 +27,7 @@ export class Report {
 
    @Column()
    mileage: number;
+
+   @ManyToOne(() => User, user => user.reports)
+   user: User;
 }
